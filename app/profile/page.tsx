b0a309003fd27f30e98fccb6faa06a1b59f67355
asemaj <asem.yahya@hotmail.com>
2024-04-08 21:37:37 +0300

    asdas

---
+++ app/profile/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Profile from '@/components/Profile'
+import {useState}  from 'react';
+function profile() {
+  return (
+    <>
+    <Profile />
+    </>
+  )
+}
+
+export default profile;
+
+
---
+++ app/trips/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Trips from '@/components/Trips'
+function trips() {
+  return (
+    <>
+    <Trips />
+    </>
+  )
+}
+
+export default trips
