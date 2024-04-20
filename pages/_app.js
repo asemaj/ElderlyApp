644dd2c271b391b6a0ceef936772c74a7fd87400
asemaj <asem.yahya@hotmail.com>
2024-04-20 11:53:36 +0300

    fixing the aws-exports issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../app/globals.css'
 import { withAuthenticator } from '@aws-amplify/ui-react'
 import { Amplify } from 'aws-amplify'
-import awsExports from '../aws-exports' 
+import awsExports from '../aws-exports.js' 
 Amplify.configure({ ...awsExports })
 
 function MyApp ({ Component, pageProps }) {
