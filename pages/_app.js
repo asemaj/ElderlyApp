ae524ba132aad53438df828b68600e7797e710c9
asemaj <asem.yahya@hotmail.com>
2024-04-20 12:52:53 +0300

    commented the _app.js file to debugg

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
-import '../app/globals.css'; // Import global styles
-import { withAuthenticator } from '@aws-amplify/ui-react'; // Import AWS Amplify's authentication components
-import Amplify from 'aws-amplify'; // Import Amplify from aws-amplify library (no curly braces needed)
-import awsExports from '../aws-exports'; // Import AWS Amplify configuration
+// import '../app/globals.css'; // Import global styles
+// import { withAuthenticator } from '@aws-amplify/ui-react'; // Import AWS Amplify's authentication components
+// import Amplify from 'aws-amplify'; // Import Amplify from aws-amplify library (no curly braces needed)
+// import awsExports from '../aws-exports'; // Import AWS Amplify configuration
 
-Amplify.configure({ ...awsExports }); // Configure AWS Amplify with the settings from awsExports
+// // import { Amplify } from 'aws-amplify';
+// import config from './amplifyconfiguration.json';
+// Amplify.configure(config);
 
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
-}
+// // Amplify.configure({ ...awsExports }); // Configure AWS Amplify with the settings from awsExports
 
-export default withAuthenticator(MyApp); // Wrap your app with authentication components
+// function MyApp({ Component, pageProps }) {
+//     return <Component {...pageProps} />;
+// }
+
+// export default withAuthenticator(MyApp); // Wrap your app with authentication components
