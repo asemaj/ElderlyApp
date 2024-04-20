53c40a7e3abac98e6191a925e3c3d4550c3708e2
asemaj <asem.yahya@hotmail.com>
2024-04-20 15:38:00 +0300

    asdasd

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../app/globals.css'; // Import global styles
 import { withAuthenticator } from '@aws-amplify/ui-react'; // Import AWS Amplify's authentication components
 import {Amplify} from 'aws-amplify'; // Import Amplify from aws-amplify library (no curly braces needed)
-import awsExports from '@/aws-exports'; // Import AWS Amplify configuration
+import awsExports from '../src/aws-exports'; // Import AWS Amplify configuration
 // import config from './amplifyconfiguration.json';
 import {AppProps } from 'next/app';
 import "@aws-amplify/ui-react/styles.css"
